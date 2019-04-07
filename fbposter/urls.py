534c5b8d825e4f8c9730ade950cf0a4899a30404
Pabin R Luitel <pabin2050@gmail.com>
2019-04-07 17:20:56 +0545

    admin url commented

--- fbposter/urls.py
+++ fbposter/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
     path('post/', include('poster.urls')),
